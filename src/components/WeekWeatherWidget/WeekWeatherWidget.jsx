e98e2556f0709778680a0b348bee5eab612d3640
alextheoctopus <sashabeznosova2002@mail.ru>
2024-04-16 11:51:06 +0400

    fixed graphics

--- src/components/WeekWeatherWidget/WeekWeatherWidget.jsx
+++ src/components/WeekWeatherWidget/WeekWeatherWidget.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Stack, ThemeProvider } from "@mui/material";
+import { Box, Typography, Stack, ThemeProvider, Button } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { createTheme } from '@mui/material/styles';
 import { updateData5 } from "../../store/features/fiveDaysForecast";
@@ -18,43 +18,46 @@ theme.typography.h2 = {
 };
 
 export const WeekWeatherWidget = () => {
-    
+    const dispatch = useDispatch();
     const fiveDaysForecastRedux = useSelector(state => state.fiveDaysForecast)
     let boxStyle = {
         backgroundColor: "rgba(249, 241, 250, 0.51)",
         margin: "auto",
         marginTop: "3%",
         width: "80%",
-        height: "260px",
+        height: "300px",
         borderRadius: 5
     }
+    console.log(fiveDaysForecastRedux.fiveDaysData)
 
     return (
         <Stack direction="column">
+            <Button sx={{ height: "100px" }} onClick={() => dispatch(updateData5())} ></Button>
             {fiveDaysForecastRedux.fiveDaysData.map((day, index) => {
-                return <Box sx={boxStyle} key={index} padding={"auto"}>
-                    <Typography>{day.day}</Typography>
-                    <ResponsiveContainer width="97%" height="100%">
-                        <LineChart
-                            width={"100%"}
-                            height={"100%"}
-                            data={day}
-                            margin={{
-                                top: 10,
-                                right: 0,
-                                left: -25,
-                                bottom: 0,
-                            }}
-                        >
-                            <CartesianGrid vertical={true} strokeDasharray="1 1"></CartesianGrid>
-                            <XAxis dataKey="hour" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Line type="monotone" dataKey="temperature" stroke="#84d89d" dot={{ r: 8 }} />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </Box>
+                console.log("day: ",day[index].min);
+                return (
+                    <Box sx={boxStyle} key={index} padding={"auto"}>
+                        <ResponsiveContainer width="97%" height="91%">
+                            <LineChart
+                                width={"100%"}
+                                height={"100%"}
+                                data={day}
+                                margin={{
+                                    top: 10,
+                                    right: 0,
+                                    left: -25,
+                                    bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid vertical={true} strokeDasharray="0.5 0.5"></CartesianGrid>
+                                <XAxis dataKey="hour" strokeWidth={1} />
+                                <YAxis domain={[day[index].min, day[index].max]} includeHidden />
+                                <Tooltip />
+                                <Legend />
+                                <Line type="monotone" dataKey="temperature" stroke="#84d89d" dot={{ r: 8 }} />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </Box>)
             })}
         </Stack>
     )
--- src/store/features/fiveDaysForecast.js
+++ src/store/features/fiveDaysForecast.js
@@ -18,7 +18,7 @@ export const fetchFiveDaysForecast = createAsyncThunk('fetchFiveDaysForecast', a
 export const fiveDaysForecast = createSlice({
     name: "fiveDaysForecast",
     initialState: {
-        data: null,
+        data: null || JSON.parse(localStorage.getItem("data5")),
         loading: false,
         fiveDaysData: JSON.parse(localStorage.getItem("5daysWeather")) || {},
         error: null,
@@ -27,35 +27,70 @@ export const fiveDaysForecast = createSlice({
     reducers: {
         updateData5: (state) => {
             console.log("updateData5", state.data);
+            let tempRange = [];//массив всех температур для определения шкалы
             state.fiveDaysData = []
             if (state.error === null && state.loading === false) {
-                let localStorageList = [];
-                state.data.list.forEach((timestamp) => {
-                    let params = {
-                        temperature: null,
-                        precipitations: [],
-                        probabilityOfPerc: null,
-                        day: null,
-                        hour: null,
-                        pop: null
-                    };
-                    Math.round(timestamp.main.temp - 273) >= 0 ?
-                        params.temperature = "+" + Math.round(timestamp.main.temp - 273) :
-                        params.temperature = Math.round(timestamp.main.temp - 273)
-                    params.pop = timestamp.pop * 100 + "% ";
-                    timestamp.weather.forEach((condition) => params.precipitations.push(condition));//состояние на данный момент
-                    params.day = timestamp.dt_txt.slice(0, 10);
-                    params.hour = timestamp.dt_txt.slice(11, 13);
-                    localStorageList.push(params);
-                });
+                let fiveDaysList = [];// массив 5 дней
+                let localStorageList = [];// массив 5 дней обработанный
 
-                for (let i = 0; i < localStorageList.length; i += 8) {
-                    state.fiveDaysData.push(localStorageList.slice(i, i + 8));
+
+                let tempRange = [];// массив температур за день
+                for (let i = 0; i < state.data.list.length; i += 8) {
+                    fiveDaysList.push(state.data.list.slice(i, i + 8))
                 }
-                console.log("updateData5", state.fiveDaysData);
+                fiveDaysList.forEach((day, index) => {
+                    let dayArray = [
+                        //объекты каждые три часа и объект с минимальной и максимальной температурой
+                    ];// массив данных 24 часов
+                    day.forEach((threeHour) => {
+                        let params = {
+                            temperature: null,
+                            day: null,
+                            hour: null,
+                        };
+                        if (Math.round(threeHour.main.temp - 273) >= 0) {
+                            params.temperature = "+" + Math.round(threeHour.main.temp - 273)
+                            tempRange.push(parseInt(params.temperature));
+                        } else {
+                            params.temperature = Math.round(threeHour.main.temp - 273);
+                            tempRange.push(parseInt(params.temperature));
+                        }
+                        params.day = updateDate(threeHour.dt_txt.slice(0, 10));
+                        params.hour = threeHour.dt_txt.slice(11, 13);
+                        dayArray.push(params);
+                        console.log(dayArray, index);
+                    });
+                    let min = tempRange.reduce((a, b) => {
+                        return Math.min(a, b);
+                    });
+                    let max = tempRange.reduce((a, b) => {
+                        return Math.max(a, b);
+                    });
+                    dayArray.forEach((timeStamp) => {
+                        timeStamp.min = min;
+                        timeStamp.max = max;
+                    });
 
+                    console.log(dayArray, index);
+                    state.fiveDaysData.push(dayArray);
+                });
                 localStorage.setItem('5daysWeather', JSON.stringify(state.fiveDaysData));
             }
+        },
+        updateDate:(state,action)=>{
+            const dateStr = action.payload;
+            const date = new Date(dateStr);
+
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const dayOfWeek = daysOfWeek[date.getDay()];
+const dayOfMonth = date.getDate();
+const month = months[date.getMonth()];
+
+const formattedDate = `${dayOfWeek} ${dayOfMonth} of ${month}`;
+
+return formattedDate; 
         }
     },
     extraReducers: (builder) => {
@@ -68,9 +103,8 @@ export const fiveDaysForecast = createSlice({
                 state.loading = false;
                 state.data = action.payload;
                 console.log(action.payload, "jdjd");
-                // fiveDaysForecast.caseReducers.test(state,action);
-
-                fiveDaysForecast.caseReducers.updateData5(state,action);
+                localStorage.setItem("data5", JSON.stringify(action.payload));
+                fiveDaysForecast.caseReducers.updateData5(state, action);
             })
             .addCase(fetchFiveDaysForecast.rejected, (state, action) => {
                 state.loading = false; //и если ошибка, то мы выводим и ошибку(офлайн) и последние данные из локалсторедж
@@ -78,5 +112,39 @@ export const fiveDaysForecast = createSlice({
             });
     }
 });
-export const { updateData5 } = fiveDaysForecast.actions;
+export const { updateData5,updateDate } = fiveDaysForecast.actions;
 export default fiveDaysForecast.reducer;
+// state.data.list.forEach((timestamp, index) => {
+                //     let params = {
+                //         min: null,
+                //         max: null,
+                //         temperature: null,
+                //         precipitations: [],
+                //         probabilityOfPerc: null,
+                //         day: null,
+                //         hour: null,
+                //     };
+                //     if (Math.round(timestamp.main.temp - 273) >= 0) {
+                //         params.temperature = "+" + Math.round(timestamp.main.temp - 273)
+                //         tempRange.push(parseInt(params.temperature));
+                //     } else {
+                //         params.temperature = Math.round(timestamp.main.temp - 273);
+                //         tempRange.push(parseInt(params.temperature));
+                //     }
+                //     // params.min = tempRange.reduce((a, b) => {
+                //     return Math.min(a, b);
+                // }); 
+                // params.max = tempRange.reduce((a, b) => {
+                //     return Math.max(a, b);
+                // }); 
+                //     timestamp.weather.forEach((condition) => params.precipitations.push(condition));//состояние на данный момент
+                //     params.day = timestamp.dt_txt.slice(0, 10)
+                //     params.hour = timestamp.dt_txt.slice(11, 13);
+                //     localStorageList.push(params);
+                // });
+
+                // for (let i = 0; i < localStorageList.length; i += 8) {
+                //     state.fiveDaysData.push(localStorageList.slice(i, i + 8));
+                // }
+                // console.log("updateData5", state.fiveDaysData);
+                // console.log("params", tempRange)
